f51033d77bf994e7db7d289afb48ed21f1f0af61
Brianna Doubt <bri@briannadoubt.com>
2022-02-05 16:04:05 -0800

    Initial Commit

---
+++ Shared/TicTacTofuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacTofuApp.swift
+//  Shared
+//
+//  Created by Bri on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacTofuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
