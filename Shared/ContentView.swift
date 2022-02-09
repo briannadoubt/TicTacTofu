bfeafc19ee48adf2bbc74a207b3da85fc2f69ad2
Brianna Doubt <bri@briannadoubt.com>
2022-02-08 22:11:39 -0800

    Add missing prologue view (during refactor)

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                 }
                 Section {
                     NavigationLink("Anyways...") {
-                        
+                        PrologueView()
                     }
                     .foregroundColor(.accentColor)
                 }
