074c55add9cd2508a632244050c850c393e7f38f
Brianna Doubt <bri@briannadoubt.com>
2022-02-09 11:13:04 -0800

    Refactor isFull and add comments

--- Shared/Board.swift
+++ Shared/Board.swift
@@ -8,8 +8,12 @@
 import GameplayKit
 import SwiftUI
 
+/// This holds the main logic of the game, as it conforms to the `GKGameModel` protocol.
 class Board: NSObject, GKGameModel, ObservableObject {
     
+    /// Create a new board
+    /// - Parameter initialPlayer: The player who takes the first turn
+    /// - Parameter size: The size of the board, or `N` on an `NxN board`
     init(initialPlayer: Player, size: Int) {
         self.size = size
         self.currentPlayer = GamePlayer(player: initialPlayer)
@@ -19,17 +23,30 @@ class Board: NSObject, GKGameModel, ObservableObject {
         createRows(with: size)
     }
     
+    /// The size of the board, or `N` on an `NxN board`
     let size: Int
     
+    /// Whether or not the game has ended
     @Published var state: GameState = .playing
     
+    /// The player that represents the end user
     @Published var playingAs: Player
+    
+    /// The player that represents the computer and is powered by a `GKStrategist` class on the `GameView`
     @Published var computerPlayer: Player
     
+    /// The player who's exptected to play the next move
     @Published var currentPlayer: GamePlayer
     
+    /// The rows on the board.
+    /// `Row` is just a typealias as follows:
+    /// ```
+    /// typealias Rows = [Tile]
+    /// ```
+    /// This means that an array of rows is a matrix, or a nested array. From this we can calculate all of our moves.
     @Published var rows: [Row] = []
     
+    /// Reset the game to the initial state
     func reset() {
         rows = []
         state = .playing
@@ -37,14 +54,17 @@ class Board: NSObject, GKGameModel, ObservableObject {
         createRows(with: size)
     }
     
+    /// The current players for GameplayKit
     var players: [GKGameModelPlayer]? {
         return GamePlayer.allPlayers
     }
     
+    /// The player who's exptected to play the next move (for GameplayKit)
     var activePlayer: GKGameModelPlayer? {
         currentPlayer
     }
     
+    /// Create a board given the input size
     func createRows(with size: Int) {
         var rows = [[Tile]]()
         for row in 0..<size {
@@ -56,6 +76,7 @@ class Board: NSObject, GKGameModel, ObservableObject {
         self.rows = rows
     }
     
+    /// Calculate the current frame's score
     func score(for player: GKGameModelPlayer) -> Int {
         guard let gamePlayer = player as? GamePlayer else {
             return 0
@@ -69,6 +90,7 @@ class Board: NSObject, GKGameModel, ObservableObject {
         return 0
     }
     
+    /// Apply the input game model to the current game model instance. This is for copying and modifying during recursion!
     func setGameModel(_ gameModel: GKGameModel) {
         if let board = gameModel as? Board {
             rows = board.rows
@@ -77,6 +99,7 @@ class Board: NSObject, GKGameModel, ObservableObject {
         }
     }
     
+    /// Return the possible moves given the current state of the board. This is used to determine where the computer is allowed to move next.
     func gameModelUpdates(for player: GKGameModelPlayer) -> [GKGameModelUpdate]? {
         guard let gamePlayer = player as? GamePlayer else {
             return nil
@@ -88,6 +111,8 @@ class Board: NSObject, GKGameModel, ObservableObject {
         return moves
     }
     
+    /// Once the recursion reaches an end state it needs to commit the move. Do that here!
+    /// This can be thought of as the recursion moving back up the tree
     func apply(_ gameModelUpdate: GKGameModelUpdate) {
         guard let move = gameModelUpdate as? Move else {
             fatalError()
@@ -95,10 +120,12 @@ class Board: NSObject, GKGameModel, ObservableObject {
         make(move)
     }
     
+    /// Determine if a given player has won
     func isWin(for player: GKGameModelPlayer) -> Bool {
         guard let gamePlayer = player as? GamePlayer else {
             return false
         }
+        // Gather all the possible winning positions with the current board values and check if they're all the given player.
         let allWinningPositions = allWinningPositions(on: rows)
         for position in allWinningPositions {
             guard position.allSatisfy({ $0.player != nil && $0.player == gamePlayer.player }) else {
@@ -109,10 +136,12 @@ class Board: NSObject, GKGameModel, ObservableObject {
         return false
     }
     
+    /// Modify the `rows` with the given move's position
     func make(_ move: Move) {
         rows[move.position.row][move.position.column].player = currentPlayer.player
     }
     
+    /// Conform to NSCopy for the recursion memory mapping
     func copy(with zone: NSZone? = nil) -> Any {
         let copy = Board(initialPlayer: currentPlayer.player, size: rows.count)
         copy.currentPlayer = currentPlayer
@@ -121,12 +150,15 @@ class Board: NSObject, GKGameModel, ObservableObject {
         return copy
     }
     
+    /// Loop through all
     func allWinningPositions(on board: [Row]) -> [Row] {
         
         var allWinningPositions: [Row] = []
         
+        // Matrix is already mapped in rows
         let rows = board
         
+        // Map columns by reversing the row and column indexes
         var columns: [Row] = []
         for row in 0..<board.count {
             for column in 0..<board.count {
@@ -138,9 +170,11 @@ class Board: NSObject, GKGameModel, ObservableObject {
             }
         }
         
+        // Map diagonal (top left to bottom right)
         var diagonal: [Tile] = []
         for row in 0..<board.count {
             for column in 0..<board.count {
+                // If the row index does not equal to the column index then we're not on a relevant tile
                 if row != column {
                     continue
                 }
@@ -149,9 +183,12 @@ class Board: NSObject, GKGameModel, ObservableObject {
             }
         }
         
+        // Map antidiagonal (top right to bottom left)
         var antidiagonal: [Tile] = []
         for row in 0..<board.count {
             for column in 0..<board.count {
+                // I love math
+                // This one was real easy to think about once I put it on my whiteboard.
                 if row + column != board.count - 1 {
                     continue
                 }
@@ -168,23 +205,15 @@ class Board: NSObject, GKGameModel, ObservableObject {
         return allWinningPositions
     }
     
+    /// Check whether or not there are any more available moves
     func isFull(_ boardSize: Int) -> Bool {
-        for row in 0..<boardSize {
-            for column in 0..<boardSize {
-                let tile = rows[row][column]
-                if tile.player == nil {
-                    return false
-                }
-            }
-        }
-        return true
+        emptyTiles.isEmpty
     }
     
+    /// All the valid tiles for future moves to be made
     var emptyTiles: [Tile] {
-        flatRows.filter({ $0.player == nil })
-    }
-    
-    var flatRows: [Tile] {
-        return rows.flatMap { $0 }
+        rows
+            .flatMap { $0 } // Flatten to one dimension
+            .filter { $0.player == nil } // Filter to only tiles without a player value
     }
 }
